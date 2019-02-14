93b1cbe88d44209acff566b4b0e619397a53e1fb
Alex Danilin <alexander.danilin@dataart.com>
2019-02-14 20:33:38 +0300

    Added buttons for prev and next dates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -31,6 +32,7 @@ import { DashboardPageComponent } from './components/dashboard/dashboard-page/da
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
+        FontAwesomeModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/components/home/timesheet-table/timesheet-table.component.ts
+++ src/app/components/home/timesheet-table/timesheet-table.component.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import { Location } from '@angular/common';
+
 import {ApiService} from '../../../services/api.service';
 import {Project} from '../../../models/Project';
 import {Timesheet} from '../../../models/Timesheet';
-
-const numberOfDates = 7;
+// Font Awesome
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-timesheet-table',
@@ -11,22 +14,39 @@ const numberOfDates = 7;
     styleUrls: ['./timesheet-table.component.less']
 })
 export class TimesheetTableComponent implements OnInit {
+    private numberOfDates: number = 7;
+    private date: string = '';
+    private prevLink: string = '';
+    private nextLink: string = '';
     private dates: string[] = [];
     private timesheets: Timesheet[] = [];
     private projects: Project[] = [];
 
-    constructor(private apiService: ApiService) {
+    // Font Awesome
+    faArrowLeft = faArrowLeft;
+    faArrowRight = faArrowRight;
+
+    constructor(
+        private apiService: ApiService,
+        private router: Router,
+        private route: ActivatedRoute,
+        private location: Location
+    ) {
     }
 
     ngOnInit() {
+        // get date from GET request
+        this.date = this.route.snapshot.queryParams.date ?
+            new Date(this.route.snapshot.queryParams.date).toISOString().substr(0, 10) :
+            new Date().toISOString().substr(0, 10);
+
         // set date from
-        let fromDate = new Date('2019-02-08');
-        this.dates.push(fromDate.toISOString().substr(0, 10));
+        this.dates.push(this.date.substr(0, 10));
         // add following dates
-        while (this.dates.length < numberOfDates) {
-            let nextDate = new Date([...this.dates].pop());
-            nextDate.setDate(nextDate.getDate() + 1);
-            this.dates = [...this.dates, nextDate.toISOString().substr(0, 10)];
+        while (this.dates.length < this.numberOfDates) {
+            let dateToAdd = new Date([...this.dates].pop());
+            dateToAdd.setDate(dateToAdd.getDate() + 1);
+            this.dates = [...this.dates, dateToAdd.toISOString().substr(0, 10)];
         }
 
         // get timesheets by dates
@@ -38,6 +58,19 @@ export class TimesheetTableComponent implements OnInit {
                 this.projects = data.slice(0, 2);
             });
         });
-    }
 
+        // set prev and next links
+        let prevDate = new Date([...this.dates].shift());
+        prevDate.setDate(prevDate.getDate() - this.numberOfDates);
+        this.prevLink = this.router.createUrlTree(
+            [this.route.snapshot.routeConfig.path],
+            { queryParams: {date: prevDate.toISOString().substr(0, 10)} }
+        ).toString();
+        let nextDate = new Date([...this.dates].pop());
+        nextDate.setDate(nextDate.getDate() + 1);
+        this.nextLink = this.router.createUrlTree(
+            [this.route.snapshot.routeConfig.path],
+            { queryParams: {date: nextDate.toISOString().substr(0, 10)} }
+        ).toString();
+    }
 }
