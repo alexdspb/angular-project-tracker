b6fd2e972977ffe4e26d743cbc8dd85f77dd4201
Alex Danilin <alexander.danilin@dataart.com>
2019-03-04 15:52:02 +0300

    Core reformatted

--- src/app/modules/user/skill-modal/skill-modal.component.ts
+++ src/app/modules/user/skill-modal/skill-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
@@ -7,56 +7,57 @@ import {Skill} from '../../../models/Skill';
 import {Employee} from '../../../models/Employee';
 
 @Component({
-  selector: 'app-skill-modal',
-  templateUrl: './skill-modal.component.html',
-  styleUrls: ['./skill-modal.component.less']
+    selector: 'app-skill-modal',
+    templateUrl: './skill-modal.component.html',
+    styleUrls: ['./skill-modal.component.less']
 })
 export class SkillModalComponent implements OnInit {
-  skill: Skill;
-  employee: Employee;
-  // modal form
-  skillForm = new FormGroup({
-    EmployeeId: new FormControl('', [Validators.required]),
-    SkillId: new FormControl('', [Validators.required]),
-    LevelId: new FormControl('', [Validators.required]),
-  });
-  loading = false;
-  submitted = false;
-  error = false;
+    skill: Skill;
+    employee: Employee;
+    // modal form
+    skillForm = new FormGroup({
+        EmployeeId: new FormControl('', [Validators.required]),
+        SkillId: new FormControl('', [Validators.required]),
+        LevelId: new FormControl('', [Validators.required]),
+    });
+    loading = false;
+    submitted = false;
+    error = false;
 
-  constructor(
-      private activeModal: NgbActiveModal,
-      private apiService: ApiService,
-  ) { }
-
-  ngOnInit() {
-    console.log(this.skill);
-    // reformat skill to form values
-    const controls = {
-      SkillId: this.skill.Id,
-      LevelId: this.skill.LevelId,
-      EmployeeId: this.skill.LevelId,
-    };
-    this.skillForm.setValue(controls);
-  }
-
-  // convenience getter for easy access to form fields
-  get f() {
-    return this.skillForm.controls;
-  }
-
-  onSave(form) {
-    console.log(form);
-    this.submitted = true;
-
-    if (this.skillForm.invalid) {
-      return;
+    constructor(
+        private activeModal: NgbActiveModal,
+        private apiService: ApiService,
+    ) {
     }
 
-    this.loading = true;
-    // todo: save or create skill
-    setTimeout(() => {
-      this.activeModal.close();
-    }, 1000);
-  }
+    ngOnInit() {
+        console.log(this.skill);
+        // reformat skill to form values
+        const controls = {
+            SkillId: this.skill.Id,
+            LevelId: this.skill.LevelId,
+            EmployeeId: this.skill.LevelId,
+        };
+        this.skillForm.setValue(controls);
+    }
+
+    // convenience getter for easy access to form fields
+    get f() {
+        return this.skillForm.controls;
+    }
+
+    onSave(form) {
+        console.log(form);
+        this.submitted = true;
+
+        if (this.skillForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        // todo: save or create skill
+        setTimeout(() => {
+            this.activeModal.close();
+        }, 1000);
+    }
 }
